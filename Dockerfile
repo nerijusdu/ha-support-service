c413cf27d41298ecfaaf49555400d0c786b32bf9
Nerijus <dnerijus72@gmail.com>
2022-02-06 22:36:09 +0200

    host local files

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ COPY go.mod go.sum ./
 RUN go mod download && go mod verify
 
 COPY . .
-RUN go build -v -o /ha-tv-service
+RUN go build -v -o /ha-support-service
 
 EXPOSE 3000
 
-CMD ["/ha-tv-service"]
+CMD ["/ha-support-service"]
--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"haservice/config"
 	"haservice/routes"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	config := config.GetConfig()
 	r := mux.NewRouter()
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
 	routes.RegisterRoutes(r)
 
-	url := fmt.Sprintf("127.0.0.1:%d", config.Port)
+	serverUrl := os.Getenv("SERVER_URL")
+	url := fmt.Sprintf("%s:%d", serverUrl, config.Port)
 	fmt.Println("Server started on " + url)
 	http.ListenAndServe(url, r)
 }
