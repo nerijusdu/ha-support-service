408bc83c2bb5019b4895021bdf87bbfb90ef7593
Nerijus <dnerijus72@gmail.com>
2022-02-13 21:31:59 +0200

    add logging

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.17
 require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/tidwall/gjson v1.14.0 // indirect
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"haservice/config"
 	"haservice/routes"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
@@ -16,11 +17,15 @@ func main() {
 	godotenv.Load()
 	config := config.GetConfig()
 	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("This is a catch-all route"))
+	})
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 	routes.RegisterRoutes(r)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
 
 	serverUrl := os.Getenv("SERVER_URL")
 	url := fmt.Sprintf("%s:%d", serverUrl, config.Port)
 	fmt.Println("Server started on " + url)
-	http.ListenAndServe(url, r)
+	http.ListenAndServe(url, loggedRouter)
 }
